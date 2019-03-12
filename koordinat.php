38044c3ffc6c02fa4ce1b4ea416853bfe4eb00e5
Ole Tjernstad <ott556@gmail.com>
2019-03-12 22:00:01 +0100

    kode som på server

---
+++ koordinat.php
@@ -0,0 +1,25 @@
+﻿<?php
+$runder = 12;
+
+if(empty($_POST['round']))
+{
+    echo 'N 66 14.666<br> E011 12.543';
+    die();
+}
+
+if(count($_POST['sequence']) != $runder)
+{
+    echo 'N 66 14.666<br> E011 12.543';
+    die();
+}
+
+if($_POST['round'] != $runder)
+{
+    echo 'N 66 14.666<br> E011 12.543';
+    die();
+}
+
+ echo 'N 60 12.121<br> E011 52.077';  
+ die();
+
+?>
